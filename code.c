79e5cc9c6d4ed521d9898d210e0dd8b71fd6ce03
debasishdash01 <ddashdebasishdash@gmail.com>
2019-04-21 18:06:13 +0530

    main code

--- code.c
+++ code.c
@@ -1,11 +1,15 @@
+//gcc code.c -lpthread
+//./a.out
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<pthread.h>
 #include<semaphore.h>
 
-int i;
-sem_t s1,s2;
+int i; //shared variable
+sem_t s1,s2; //semaphore variables
 
 void *Thread1(){
 sem_wait(&s1);
@@ -28,7 +32,7 @@ sem_post(&s1);
 void *Thread3(){
 sem_wait(&s2);
 printf("I am third thread\n");
-for(int i=20;i<30;i++){
+for(int i=20;i<31;i++){
 printf("%d\n",i);
 }
 }
@@ -37,11 +41,20 @@ int main()
 {
 sem_init(&s1,0,0);
 sem_init(&s2,0,0);
+//1st argument for the address of the semaphore variable
+//2nd arg for the thread, non-zero value is used in case for process
+//3rd arg for initializing the semaphore value
+
 pthread_t tid,tid2,tid3;
 
 pthread_create(&tid,NULL,Thread1,(void *)&tid);
 pthread_create(&tid2,NULL,Thread2,(void *)&tid2);
 pthread_create(&tid3,NULL,Thread3,(void *)&tid3);
+
+sem_destroy(&s1);
+sem_destroy(&s2);
+//for destroying the semaphores
+
 pthread_exit(NULL);
 return 0;
 }
